39abec4784f19da20c44e68dc37d52c3ca1426be
Sagar Subedi <sagarxic@gmail.com>
2025-03-14 00:02:23 +0545

    use api gateway url from environment variable

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -1,3 +1,3 @@
-export const BACKEND_API_GATEWAY_URL = 'http://localhost:8765';
+export const BACKEND_API_GATEWAY_URL = process.env.NEXT_PUBLIC_API_GATEWAY_URL;
 export const APP_CLIENT_ID = '93ed453e-b7ac-4192-a6d4-c45fae0d99ac';
-export const APP_CLIENT_SECRET = 'client.devd123';
+export const APP_CLIENT_SECRET = 'client.sagar123';
