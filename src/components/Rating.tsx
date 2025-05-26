ea7245a539a7c7b674fd064cb0cec16e0bb79b7e
Sagar Subedi <sagarxic@gmail.com>
2025-05-27 00:13:40 +0545

    Change rating component

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,30 +1,34 @@
 import React from 'react';
+import { Box } from '@mui/material';
+import { Star, StarHalf, StarBorder } from '@mui/icons-material';
 
-const Rating = ({ value, text, color }) => {
+const Rating = ({ value, text, color = '#FFD700' }) => {
   return (
-    <div className='rating'>
-      <span>
-        <i style={{ color }} className={value >= 1 ? 'fas fa-star' : value >= 0.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
-      </span>
-      <span>
-        <i style={{ color }} className={value >= 2 ? 'fas fa-star' : value >= 1.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
-      </span>
-      <span>
-        <i style={{ color }} className={value >= 3 ? 'fas fa-star' : value >= 2.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
-      </span>
-      <span>
-        <i style={{ color }} className={value >= 4 ? 'fas fa-star' : value >= 4.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
-      </span>
-      <span>
-        <i style={{ color }} className={value >= 5 ? 'fas fa-star' : value >= 3.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i>
-      </span>
-      {text && <span className='mx-2'>[{text && text}]</span>}
-    </div>
+    <Box display="flex" alignItems="center">
+      <Box display="flex" color={color}>
+        <span>
+          {value >= 1 ? <Star /> : value >= 0.5 ? <StarHalf /> : <StarBorder />}
+        </span>
+        <span>
+          {value >= 2 ? <Star /> : value >= 1.5 ? <StarHalf /> : <StarBorder />}
+        </span>
+        <span>
+          {value >= 3 ? <Star /> : value >= 2.5 ? <StarHalf /> : <StarBorder />}
+        </span>
+        <span>
+          {value >= 4 ? <Star /> : value >= 3.5 ? <StarHalf /> : <StarBorder />}
+        </span>
+        <span>
+          {value >= 5 ? <Star /> : value >= 4.5 ? <StarHalf /> : <StarBorder />}
+        </span>
+      </Box>
+      {text && <Box ml={1}>{text}</Box>}
+    </Box>
   );
 };
 
 Rating.defaultProps = {
-  color: '#fee825'
+  color: '#fee825',
 };
 
 export default Rating;
